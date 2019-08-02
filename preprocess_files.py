fc9e05780b0d29f5bd6796ac7356eadefad6dd35
André Tuchapski <tuchapski@gmail.com>
2019-08-01 23:17:04 -0300

    Working on classes process_files

--- preprocess_files.py
+++ preprocess_files.py
@@ -1,22 +1,30 @@
-import os, glob
+import os, fnmatch
 
 class Preprocess:
     
-    def __init__(self):
-        if dir_path == None:
-            self.dir_path = os.getcwd()
-                
-    def scan_files(self, pattern='*.*'):
-        os.chdir(dir_path)
-        self.pattern = pattern
-        return glob.glob(os.path.join(self.dir_path, self.extention))
+    def __init__(self, base_dir=None):
+        self.base_dir = base_dir
+        if self.base_dir == None:
+            self.base_dir = os.getcwd()
+            
+    def list_from_dir(self, folder=''):
+        self.folder = os.path.join(self.base_dir, folder)
+        self.found_files = []          
+        for self.filename in os.listdir(self.folder):
+#           if os.path.isfile(os.path.join(self.folder, self.filename)):
+            self.found_files.append(self.filename)
+        return self.folder, self.found_files   
     
-    def 
-        
-    def summary(self):
-        print('Summary')
-        print('files_path: {0}'.format(self.dir_path))
-        print('n_files: {0}'.format(len(self.scanned_files))) 
+    def list_from_dir_recursive(self, folder=''):
+        self.folder = folder
+        self.found_files = []        
+        for self.d in os.walk(os.path.join(self.base_dir, folder)):        
+            self.found_files.append(self.d)
+        return self.found_files
+    
+    def search_files(self, pattern='*.*', recursive='n'):
+        self.pattern = pattern
+        self.recursive = recursive
         
     """def rename_files(self, prefix='file_', extention):
         pass
@@ -38,11 +46,7 @@ class Preprocess:
                 # rename all the files 
                 os.rename(src, dst) 
                 i += 1"""
-                
+            
+yum = Preprocess()
 
-
-dir_path = os.getcwd()
-
-found = glob.glob(('*.*'))
-
-print(found)
+yum.list_files_from_dir()
